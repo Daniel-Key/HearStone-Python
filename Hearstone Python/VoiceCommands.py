92210d545430528f556c9a54a55363be033d4cc2
Daniel-Key <dgk2@st-andrews.ac.uk>
2019-12-30 17:57:57 +0000

    Ctrl F change for Voice commands/recognition

--- Hearstone Python/VoiceCommands.py
+++ Hearstone Python/VoiceCommands.py
@@ -8,7 +8,6 @@ programRunning = True
 #
 
 def quitProgram():
-    #Python globals are broken on a whole new level of broken
     VoiceCommands.programRunning = False
 
 # 
@@ -16,7 +15,7 @@ def quitProgram():
 #
 
 def endTurn():
-    mouseControl.clickFraction(0.82, 0.45)
+    MouseControl.clickFraction(0.82, 0.45)
 
 # 
 # Menu commands
@@ -24,64 +23,64 @@ def endTurn():
 
 # Navigate to the single player hero selection screen
 def singlePlayer():
-    mouseControl.clickFraction(0.5, 0.35)
+    MouseControl.clickFraction(0.5, 0.35)
 
 def selectDifficulty(difficulty):
     if (difficulty == "normal"):
-        mouseControl.clickFraction(0.75, 0.2)
+        MouseControl.clickFraction(0.75, 0.2)
     else:
-        mouseControl.clickFraction(0.75, 0.27)
+        MouseControl.clickFraction(0.75, 0.27)
     time.sleep(0.1)
-    mouseControl.clickFraction(0.75, 0.85)
+    MouseControl.clickFraction(0.75, 0.85)
     time.sleep(1)
 
 # Select deck
 def selectDeck(number):
     if (number == 1):
-        mouseControl.clickFraction(0.25, 0.25)
+        MouseControl.clickFraction(0.25, 0.25)
     elif (number == 2):
-        mouseControl.clickFraction(0.4, 0.25)
+        MouseControl.clickFraction(0.4, 0.25)
     elif (number == 3):
-        mouseControl.clickFraction(0.5, 0.25)
+        MouseControl.clickFraction(0.5, 0.25)
     elif (number == 4):
-        mouseControl.clickFraction(0.25, 0.45)
+        MouseControl.clickFraction(0.25, 0.45)
     elif (number == 5):
-        mouseControl.clickFraction(0.4, 0.45)
+        MouseControl.clickFraction(0.4, 0.45)
     elif (number == 6):
-        mouseControl.clickFraction(0.5, 0.45)
+        MouseControl.clickFraction(0.5, 0.45)
     elif (number == 7):
-        mouseControl.clickFraction(0.25, 0.7)
+        MouseControl.clickFraction(0.25, 0.7)
     elif (number == 8):
-        mouseControl.clickFraction(0.4, 0.7)
+        MouseControl.clickFraction(0.4, 0.7)
     elif (number == 9):
-        mouseControl.clickFraction(0.5, 0.7)
+        MouseControl.clickFraction(0.5, 0.7)
         time.sleep(0.1)
         
-    mouseControl.clickFraction(0.75, 0.85)
+    MouseControl.clickFraction(0.75, 0.85)
         
 
 # Select enemy
 def selectOpponent(number):
     if (number == 1):
-        mouseControl.clickFraction(0.75, 0.1)
+        MouseControl.clickFraction(0.75, 0.1)
     elif (number == 2):
-        mouseControl.clickFraction(0.75, 0.18)
+        MouseControl.clickFraction(0.75, 0.18)
     elif (number == 3):
-        mouseControl.clickFraction(0.75, 0.23)
+        MouseControl.clickFraction(0.75, 0.23)
     elif (number == 4):
-        mouseControl.clickFraction(0.75, 0.28)
+        MouseControl.clickFraction(0.75, 0.28)
     elif (number == 5):
-        mouseControl.clickFraction(0.75, 0.35)
+        MouseControl.clickFraction(0.75, 0.35)
     elif (number == 6):
-        mouseControl.clickFraction(0.75, 0.42)
+        MouseControl.clickFraction(0.75, 0.42)
     elif (number == 7):
-        mouseControl.clickFraction(0.75, 0.50)
+        MouseControl.clickFraction(0.75, 0.50)
     elif (number == 8):
-        mouseControl.clickFraction(0.75, 0.55)
+        MouseControl.clickFraction(0.75, 0.55)
     elif (number == 9):
-        mouseControl.clickFraction(0.75, 0.62)
+        MouseControl.clickFraction(0.75, 0.62)
 
 
 # Play game
 def playGame():
-    mouseControl.clickFraction(0.75, 0.85)
+    MouseControl.clickFraction(0.75, 0.85)
--- Hearstone Python/VoiceRecognition.py
+++ Hearstone Python/VoiceRecognition.py
@@ -25,14 +25,14 @@ def takeInput():
     #
 
     if (phrase == "exit voice"):
-        voiceCommands.quitProgram()
+        VoiceCommands.quitProgram()
 
     # 
     # Gameplay commands
     #
 
     elif (phrase == "end turn" or phrase == "and turn" or phrase == "entertain" or phrase == "nth term" or phrase == "Anton"):
-        voiceCommands.endTurn()
+        VoiceCommands.endTurn()
 
     # 
     # Menu commands
@@ -40,77 +40,77 @@ def takeInput():
 
     # Navigate from main menu to single player difficulty selection
     elif (phrase == "single player"):
-        voiceCommands.singlePlayer()
+        VoiceCommands.singlePlayer()
 
     # Select single player difficulty
     elif ("difficulty" in phrase):
         if ("normal" in phrase):
-            voiceCommands.selectDifficulty("normal")
+            VoiceCommands.selectDifficulty("normal")
         elif ("expert" in phrase):
-            voiceCommands.selectDifficulty("expert")
+            VoiceCommands.selectDifficulty("expert")
         else:
             print("difficulty not recognised")
     
     # Select deck
     elif ("deck" in phrase or "dick" in phrase or "next" in phrase or "text" in phrase or "tech" in phrase or "EK" in phrase or "Jet" in phrase):
         if ("1" in phrase or "one" in phrase):
-            voiceCommands.selectDeck(1)
+            VoiceCommands.selectDeck(1)
         elif ("2" in phrase or "two" in phrase or "too" in phrase or "to" in phrase):
-            voiceCommands.selectDeck(2)
+            VoiceCommands.selectDeck(2)
         elif ("3" in phrase or "three" in phrase or "free" in phrase):
-            voiceCommands.selectDeck(3)
+            VoiceCommands.selectDeck(3)
         elif ("4" in phrase or "four" in phrase or "for" in phrase):
-            voiceCommands.selectDeck(4)
+            VoiceCommands.selectDeck(4)
         elif ("5" in phrase or "five" in phrase):
-            voiceCommands.selectDeck(5)
+            VoiceCommands.selectDeck(5)
         elif ("6" in phrase or "six" in phrase):
-            voiceCommands.selectDeck(6)
+            VoiceCommands.selectDeck(6)
         elif ("7" in phrase or "seven" in phrase or "for" in phrase):
-            voiceCommands.selectDeck(7)
+            VoiceCommands.selectDeck(7)
         elif ("8" in phrase or "eight" in phrase):
-            voiceCommands.selectDeck(8)
+            VoiceCommands.selectDeck(8)
         elif ("9" in phrase or "nine" in phrase):
-            voiceCommands.selectDeck(9)
+            VoiceCommands.selectDeck(9)
     elif ("dec1" in phrase or "daquan" in phrase):
-        voiceCommands.selectDeck(1)
+        VoiceCommands.selectDeck(1)
     elif ("dec2" in phrase or "bectu" in phrase):
-        voiceCommands.selectDeck(2)
+        VoiceCommands.selectDeck(2)
     elif ("dec3" in phrase or "Dec free" in phrase):
-        voiceCommands.selectDeck(3)
+        VoiceCommands.selectDeck(3)
     elif ("dec4" in phrase):
-        voiceCommands.selectDeck(4)
+        VoiceCommands.selectDeck(4)
     elif ("dec5" in phrase):
-        voiceCommands.selectDeck(5)
+        VoiceCommands.selectDeck(5)
     elif ("dec6" in phrase):
-        voiceCommands.selectDeck(6)
+        VoiceCommands.selectDeck(6)
     elif ("dec7" in phrase):
-        voiceCommands.selectDeck(4)
+        VoiceCommands.selectDeck(4)
     elif ("dec8" in phrase or "decade" in phrase):
-        voiceCommands.selectDeck(5)
+        VoiceCommands.selectDeck(5)
     elif ("dec9" in phrase):
-        voiceCommands.selectDeck(6)
+        VoiceCommands.selectDeck(6)
 
     # Select enemy
     elif ("opponent" in phrase or "appointment" in phrase or "exponent" in phrase):
         if ("1" in phrase):
-            voiceCommands.selectOpponent(1)
+            VoiceCommands.selectOpponent(1)
         elif ("2" in phrase or "two" in phrase or "to" in phrase):
-            voiceCommands.selectOpponent(2)
+            VoiceCommands.selectOpponent(2)
         elif ("3" in phrase or "three" in phrase or "free" in phrase):
-            voiceCommands.selectOpponent(3)
+            VoiceCommands.selectOpponent(3)
         elif ("4" in phrase or "four" in phrase or "for" in phrase):
-            voiceCommands.selectOpponent(4)
+            VoiceCommands.selectOpponent(4)
         elif ("5" in phrase or "five" in phrase):
-            voiceCommands.selectOpponent(5)
+            VoiceCommands.selectOpponent(5)
         elif ("6" in phrase or "six" in phrase):
-            voiceCommands.selectOpponent(6)
+            VoiceCommands.selectOpponent(6)
         elif ("7" in phrase or "seven" in phrase or "for" in phrase):
-            voiceCommands.selectOpponent(7)
+            VoiceCommands.selectOpponent(7)
         elif ("8" in phrase or "eight" in phrase):
-            voiceCommands.selectOpponent(8)
+            VoiceCommands.selectOpponent(8)
         elif ("9" in phrase or "nine" in phrase):
-            voiceCommands.selectOpponent(9)
+            VoiceCommands.selectOpponent(9)
 
     # Play game
     elif ("play game" in phrase):
-        voiceCommands.playGame()
+        VoiceCommands.playGame()
